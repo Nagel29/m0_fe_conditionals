afdc29599da078058e77059ae8e071a879cf4892
Ryan Nagel <RNagel29@gmail.com>
2022-07-09 14:58:41 -0600

    Add comparisons answers

--- comparisons.js
+++ comparisons.js
@@ -25,21 +25,27 @@ console.log("Is numberTeachers less than numberStudents?", numberTeachers < numb
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
+console.log("Is numberTeachers strictly equal to stringTeachers?", numberTeachers === stringTeachers);
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
+console.log("Is numberTeachers not equal to numberStudents?", numberTeachers !== numberStudents);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
+console.log("Is numberStudents greater than or equal to 20?", numberStudents >= 20);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
+console.log("Is numberStudents greater than or equal to 21?", numberStudents >= 21);
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
+console.log("Is numberStudents less than or equal to 21?", numberStudents <= 20);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
+console.log("is numberStudents less than or equal to 21?", numberStudents <= 21)
 // this should log: true
 
 
@@ -53,21 +59,24 @@ console.log("Is numberTeachers less than numberStudents?", numberTeachers < numb
 // Make sure YOU can explain it that way!
 
 console.log(4 < 9);
-// YOU DO: Explain.
+// This prints the result of evaluating if 4 is less than 9. This should result in true.
 
 var books = 3;
 console.log(4 < books);
-// YOU DO: Explain.
+// This assigns 3 to the variable (or assignment operator?) "books". Then it prints the result of evaluating if 4 is less than the "books" value.
+// This should result in false since "books" value is 3.
 
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
-// YOU DO: Explain.
+// Assigns 6 to the value of "friends" variable. Assigns 2 the value of "siblings" variable.
+// Prints the result of evaluating if the value of friends is greater than siblings. The result should be true.
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees != meals);
-// YOU DO: Explain.
+// Assigns 9 to the value of "attendees". Assigns 8 to the value of "meals".
+// Prints the result of evaluating if the value of attendees does not equal the value of meals. This shouls result in true.
 
 
 // #-------------------
@@ -90,15 +99,16 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+console.log(lovesToPlay && lovesTreats);
 
 // Determine if the dog loves to play and loves the dog park
-
+console.log(lovesToPlay && lovesDogPark);
 
 // Determine if the dog loves to play or loves the dog park
-
+console.log(lovesToPlay || lovesDogPark)
 
 // Determine if the dog loves to play and is a puppy
-
+var puppy = 1
+console.log(lovesToPlay && age <= puppy)
 // What did your final line of code evaluate to? Why do you think that is? Explain.
 // ANSWER:
