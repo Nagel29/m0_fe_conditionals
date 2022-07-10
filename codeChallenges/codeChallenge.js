aa725c5b1fad0878f5144c1df38930f0c8044573
Ryan Nagel <RNagel29@gmail.com>
2022-07-09 18:23:40 -0600

    Add FizzBuzz

--- codeChallenges/codeChallenge.js
+++ codeChallenges/codeChallenge.js
@@ -3,3 +3,13 @@ var num = 2
 
 if (num % 2 === 0) {console.log("even")}
 else if (num % 2 !== 0) {console.log("odd")};
+
+
+
+//Medium Challenge
+var num2 = 15
+
+if (num2 % 3 === 0 && num2 % 5 === 0){console.log("FizzBuzz")}
+else if (num2 % 3 === 0){console.log("Fizz")}
+else if (num2 % 5 === 0){console.log("Buzz")}
+else {console.log(num2)};
